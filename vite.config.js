54af6532d09393449b185f08276db211877e548d
David Adreani <david.adreani.da@gmail.com>
2026-07-10 22:33:39 -0400

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/course_js/',
+    build: {
+        outDir: 'docs', // Le dice a Vite que guarde el build en 'docs' en lugar de 'dist'
+    }
 })
