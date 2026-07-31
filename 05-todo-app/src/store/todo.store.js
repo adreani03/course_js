bcea0d7da291a8ff70026ebe44054091792c90dd
DAVID ADREANI <gestion.datos02@grupo-amanecer.com>
2026-07-31 16:56:18 -0400

    add

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -22,6 +22,74 @@ const initStore = () => {
 }
 
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter((todo) => todo.done)
+            
+        case Filters.Pending:
+            return state.todos.filter((todo) => !todo.done);
+
+        default:
+            throw new Error(`Option ${filter} is not valid`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    if (!description) throw new Error('Description is required');
+
+    state.todos.push(new Todo(description));
+}
+
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    if (!todoId) throw new Error('TodoId is required');
+
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    if(!Object.keys(Filters).includes(newFilter)){
+        console.warn(`El filtro ${newFilter} no es valido`)
+    }
+
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
